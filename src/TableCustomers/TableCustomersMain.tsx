172c4fe72811de4e5ed5378a8fe076e64c7589d4
Marcos Carvalho <marcoscarvalhodev99@gmail.com>
2025-08-06 23:40:06 -0300

    feat: add animations of table 3

--- src/TableCustomers/TableCustomersMain.tsx
+++ src/TableCustomers/TableCustomersMain.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { TableCustomers1 } from './TableCustomers1'
+import React from 'react';
+import { TableCustomers1 } from './TableCustomers1';
+import { TableCustomers3 } from './TableCustomers3';
 
 const TableCustomersMain = () => {
   return (
     <>
       <TableCustomers1 />
+      <TableCustomers3 />
     </>
-  )
-}
+  );
+};
 
-export default TableCustomersMain
+export default TableCustomersMain;
